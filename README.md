a02bb5ff75eaa5aa62f346163d6e2fcec1ec60b6
Bettina Steger <betty@lingohub.com>
2016-05-23 21:01:16 +0200

    update component and add readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# angularCroppie - A Javascript Image Cropper using [Croppie](https://github.com/Foliotek/Croppie)
+
+
+## Install
+Bower: `bower install angular-croppie`
+
+Npm: `npm install angular-croppie`
+
+## Usage
+
+```
+<croppie src="cropped.source" ng-model="cropped.image"></croppie>
+```
+
+## Documentation
+[Documentation](http://foliotek.github.io/Croppie#documentation)
+
--- angular-croppie.js
+++ angular-croppie.js
@@ -1,4 +1,31 @@
 angular.module('angularCroppie', []).
-  component('angularCroppie', function () {
-    // implementation goes here
+  component('croppie', function () {
+    bindings: {
+      src: '=',
+      ngModel: '='
+    },
+    controller: function ($scope, $element) {
+      var ctrl = this;
+
+      if(!ctrl.src) { return; }
+
+      var c = new Croppie($element[0], {
+        viewport: {
+          width: 200,
+          height: 200
+        },
+        update: function () {
+          c.result('canvas').then(function(img) {
+            $scope.$apply(function () {
+              ctrl.ngModel = img;
+            });
+          });
+        }
+      });
+
+      // bind an image to croppie
+      c.bind({
+        url: ctrl.src
+      });
+    }
   });
