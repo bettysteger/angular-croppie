b23292b4dc836db2f8d0748df0dd6141243da3ee
Nagy Gergely <nagy.gergely14@gmail.com>
2017-02-23 12:51:55 +0100

    Testing #1

--- angular-croppie.js
+++ angular-croppie.js
@@ -1,34 +1,59 @@
-angular.module('angularCroppie', []).
+(function() {
+
+  "use strict";
+
+  angular.module('angularCroppie', []).
   component('croppie', {
+    controller: AngularCroppie,
     bindings: {
       src: '<',
-      ngModel: '='
-    },
-    controller: function ($scope, $element) {
-      var ctrl = this;
-
-      var c = new Croppie($element[0], {
-        viewport: {
-          width: 200,
-          height: 200
-        },
-        update: function () {
-          c.result('canvas').then(function(img) {
-            $scope.$apply(function () {
-              ctrl.ngModel = img;
-            });
-          });
-        }
-      });
-
-      ctrl.$onChanges = function (changesObj) {
-        var src = changesObj.src && changesObj.src.currentValue;
-        if(src) {
-          // bind an image to croppie
-          c.bind({
-            url: src
-          });
-        }
-      };
+      ngModel: '=',
+      onChange: '&',
+      options: '@',
+      resultConfig: '<'
     }
   });
+  AngularCroppie.$inject = ['$scope', '$element'];
+
+
+  /*@ngInject*/
+  function AngularCroppie($scope, $element) {
+    var ctrl = this;
+    var c;
+
+
+
+    ctrl.$onInit = function() {
+      //Append/Override update method in 
+      vm.options.update = onCroppieUpdate;
+      c = new Croppie($element[0],
+        vm.options);
+    };
+
+    ctrl.$onChanges = function(changesObj) {
+      var src = changesObj.src && changesObj.src.currentValue;
+      if (src) {
+        // bind an image to croppie
+        c.bind({
+          url: src
+        });
+      }
+    };
+
+    ctrl.$onDestroy = function() {
+      if (c) {
+        c.destroy();
+      }
+    };
+
+
+    function onCroppieUpdate() {
+      c.result(vm.resultConfig).then(function(img) {
+        $scope.$apply(function() {
+          ctrl.ngModel = img;
+        });
+      });
+    }
+  }
+
+})();
